102b3d72ba855a8f434849068d66ea0796fe7730
FerchoJpt-Controlc <jesustarax6@gmail.com>
2025-08-22 09:56:47 -0600

    menu

--- main.py
+++ main.py
@@ -29,3 +29,43 @@ class Producto(I_Producto):
     def __str__(self):
         return f"{self.nombre} (ID:{self.IDproducto}, Precio: {self.precio}, Cat:{self.IDcategoria})"
 
+def menu():
+    inventarios={}
+
+    while True:
+        print("\n==M E N U==")
+        print("1. Ingresar nuevo producto")
+        print("2. Mostrar inventario")
+        print("3. Salir")
+
+        opcion = input("Seleccione una opcion: ")
+
+        match opcion:
+            case "1":
+                try:
+                    idproducto = int(input("ID Producto: "))
+                    nombre = input("Nombre: ")
+                    precio = float(input("Precio: "))
+                    idcategoria = int(input("ID Categoría: "))
+
+                    produ = Producto(idproducto, idcategoria, nombre, precio)
+
+                    print("PRODUCTO REGISTRADO CON EXITO")
+                except ValueError:
+                    print("ERROR...ENTRADA INVALIDA...")
+
+            case "2":
+                if inventarios:
+                    print("\n_-INVENTARIO-_")
+
+                else:
+                    print("NO HAY PRODUCTOS REGISTRADOS")
+
+            case "3":
+                print("ADIOS...Saliendo del sistema...")
+                break
+
+            case _:
+                print("Opcion invalida...intente de nuevo")
+
+menu()
