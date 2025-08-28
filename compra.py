0f68d401c78164d6a58ac502610290ed3b59a1a8
FerchoJpt-Controlc <jesustarax6@gmail.com>
2025-08-28 11:28:46 -0600

    classe para detallescompra

--- compra.py
+++ compra.py
@@ -11,3 +11,17 @@ class Compra:
 
     def __str__(self):
         return f"Compra(ID:{self.IDCompra}, Fecha:{self.FechaIngreso}, Empleado:{self.IDEmpleado}, NIT:{self.NIT}, Total:{self.Total})"
+
+
+class DetalleCompra:
+    def __init__(self, IDDetalleCompra, IDVenta, IDProducto, Cantidad, Subtotal, FechaCaducidad):
+        self.IDDetalleCompra = IDDetalleCompra
+        self.IDVenta = IDVenta
+        self.IDProducto = IDProducto
+        self.Cantidad = Cantidad
+        self.Subtotal = Subtotal
+        self.FechaCaducidad = FechaCaducidad
+
+    def __str__(self):
+        return f"Detalle(ID:{self.IDDetalleCompra}, Venta:{self.IDVenta}, Prod:{self.IDProducto}, Cantidad:{self.Cantidad}, Subtotal:{self.Subtotal}, Vence:{self.FechaCaducidad})"
+
