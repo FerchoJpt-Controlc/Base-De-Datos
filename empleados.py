3d98364d563baf7a9b4652ced0da77b8c2b01d97
FerchoJpt-Controlc <jesustarax6@gmail.com>
2025-08-28 08:41:25 -0600

    constructor para empleado

---
+++ empleados.py
@@ -0,0 +1,14 @@
+class Empleado:
+    def __init__(self, IDEmpleado, Nombre, Direccion, Telefono, Correo, Puesto):
+        self.IDEmpleado = IDEmpleado
+        self.Nombre = Nombre
+        self.Direccion = Direccion
+        self.Telefono = Telefono
+        self.Correo = Correo
+        self.Puesto = Puesto
+
+    def __str__(self):
+        return (f"Empleado(ID:{self.IDEmpleado}, Nombre:{self.Nombre}, Dirección:{self.Direccion}, Teléfono:{self.Telefono}, Correo:{self.Correo}, Puesto:{self.Puesto})")
+
+
+
